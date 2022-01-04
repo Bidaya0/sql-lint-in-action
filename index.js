e4b1eccc6b6167595c1c3243a29e322cad7b7360
Bidaya0 <xianzelin@huoxian.cn>
2022-01-05 00:41:13 +0800

    add test

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ export default class index {
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-const github =
+const { exec } = require('child_process');
+const initbash = `curl https://github.com/joereynolds/sql-lint/releases/latest/download/sql-lint-linux -o  /usr/local/bin/sql-lint &&
+chmod +x /usr/local/bin/sql-lint &&
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose`
+				
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('path');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+	exec(initbash, (err, stdout, stderr) => {
+		if (err) {
+			return;
+  }
+
+		console.log(`stdout: ${stdout}`);
+		console.log(`stderr: ${stderr}`);
+	});
+	const runbash = `sqllint ${path}`
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -23,3 +33,15 @@ try {
 } catch (error) {
   core.setFailed(error.message);
 }
+//function() {
+//				exec(initbash, (err, stdout, stderr) => {
+//					if (err) {
+//						// node couldn't execute the command
+//						return;
+//					}
+//
+//					// the *entire* stdout and stderr (buffered)
+//					console.log(`stdout: ${stdout}`);
+//					console.log(`stderr: ${stderr}`);
+//				})
+//}
