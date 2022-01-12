e82308de42cb23a707a3dedc8a6ed6b60183b224
Bidaya0 <xianzelin@huoxian.cn>
2022-01-13 01:51:52 +0800

    fix ignore-errors

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ sudo ln -s /usr/local/bin/sql-lint /usr/bin/sql-lint`
 
 function initconfig(host,user,password,driver='mysql',port=3306,ignore_errors=[]){
 		config_data = {'host':host,'user':user,'password':password,'driver':driver,'port':port,'ignore-errors':ignore_errors}
+		console.log(config_data)
 		writeFileSync('/tmp/config.json',JSON.stringify(config_data),{flag: 'w',overwrite:true})
 }
 
