8b78212e337c895086166699bb6ad523e4ae2a10
Bidaya0 <xianzelin@huoxian.cn>
2022-01-13 02:13:33 +0800

    change logic

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ sudo chmod +x /usr/local/bin/sql-lint &&
 sudo ln -s /usr/local/bin/sql-lint /usr/bin/sql-lint`
 
 function initconfig(host,user,password,driver='mysql',port=3306,ignore_errors=[]){
+		if (host==''){
 		config_data = {'host':host,'user':user,'password':password,'driver':driver,'port':port,'ignore-errors':ignore_errors}
+		}else{
+		config_data = {'driver':driver,'port':port,'ignore-errors':ignore_errors}
+		}
 		console.log(config_data)
 		writeFileSync('/tmp/config.json',JSON.stringify(config_data),{flag: 'w',overwrite:true})
 }
@@ -24,6 +28,12 @@ function get_runbash(path,use_database){
 		return `sql-lint ${path} --config=/tmp/config.json`
 	}
 	return `sql-lint ${path}`
+}
+function is_use_database(host){
+	if (host == ''){
+		return false
+	}
+	return true
 }
 try {
   const path = core.getInput('path');
@@ -41,16 +51,13 @@ try {
 			}
 		});
 	}
-	const use_database = core.getBooleanInput('use_database', { required: false})
-	if (use_database == true){
-		const host = core.getInput('host', { required: false})
-		const user = core.getInput('user',{required: false})
-		const password = core.getInput('password',{required:false})
-		const driver = core.getInput('driver',{required:false})
-		const port = core.getInput('port',{required:false})
-		const ignore_errors = core.getInput('ignore_errors',{required:false}).split(',').filter((x)=>(x!=''))
-		initconfig(host,user,password,driver,port,ignore_errors)
-	}
+	const host = core.getInput('host', { required: false})
+	const user = core.getInput('user',{required: false})
+	const password = core.getInput('password',{required:false})
+	const driver = core.getInput('driver',{required:false})
+	const port = core.getInput('port',{required:false})
+	const ignore_errors = core.getInput('ignore_errors',{required:false}).split(',').filter((x)=>(x!=''))
+	initconfig(host,user,password,driver,port,ignore_errors)
 	const runbash = get_runbash(path,use_database)
 	exec(runbash, (err, stdout, stderr) => {
 		if (err) {
